4bfef9c49ddb6a736b17b2eb0225a66e3e463414
Andrew Witwicki <vitvitskiy3@gmail.com>
2022-06-28 21:45:55 +0200

    Add auth, database

---
+++ Blazor-starter/Consts.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor_starter
+{
+    public class Consts
+    {
+
+        public const string AdminRole = "Admin";
+        public const string ModeratorRole = "Moderator";
+        public const string UserRole = "User";
+
+        public static List<string> RolesList => new List<string>() { AdminRole, ModeratorRole, UserRole };
+    }
+}
--- Blazor-starter/Program.cs
+++ Blazor-starter/Program.cs
@@ -1,12 +1,28 @@
+using Blazor_starter;
 using Blazor_starter.Data;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options
+                 //.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")) Dont forget to create migration!
+                   .UseInMemoryDatabase("InMemoryDb")
+                   );
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddServerSideBlazor();
+
+
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
@@ -19,13 +35,47 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    // Automatic update database when exist new migration
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    // Migration
+    if (context.Database.IsSqlServer())
+    {
+        context.Database.Migrate();
+    }
+
+    // Init roles
+    {
+        var _roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        // Create all roles
+        foreach (var roleName in Consts.RolesList)
+        {
+            var roleExists = _roleManager.FindByNameAsync(roleName).Result;
+            if (roleExists == null)
+            {
+                _roleManager.CreateAsync(new IdentityRole(roleName)).Wait();
+            }
+        }
+    }
+}
+
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
 
 app.Run();
